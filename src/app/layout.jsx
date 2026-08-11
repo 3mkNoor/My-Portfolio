095ffc4ae4292277dd079edd65444d6c7324d761
Noor <norxxxxnor@gmail.com>
2026-08-11 12:45:37 +0300

    analytics added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Geist+Pixel:ELSH@20&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet"></link>
       </head>
-      <body className="">{children}</body>
+      <body className="">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
